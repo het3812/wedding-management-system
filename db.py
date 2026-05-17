07f122002c1b75c6408adceac81b9e2ce0b29a25
het3812 <hetjoshi3812@gmail.com>
2026-05-18 00:53:58 +0530

    Fixed lazy database connection

--- db.py
+++ db.py
@@ -4,20 +4,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-connection = mysql.connector.connect(
-    host=os.getenv("MYSQLHOST"),
-    port=int(os.getenv("MYSQLPORT")),
-    user=os.getenv("MYSQLUSER"),
-    password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE")
-)
-
-cursor = connection.cursor(dictionary=True)
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        port=int(os.getenv("MYSQLPORT")),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE")
+    )
 
 def execute_query(query, values=None):
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+
     cursor.execute(query, values or ())
-    return cursor.fetchall()
+    result = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return result
 
 def execute_update(query, values=None):
+    connection = get_connection()
+    cursor = connection.cursor()
+
     cursor.execute(query, values or ())
     connection.commit()
+
+    cursor.close()
+    connection.close()
