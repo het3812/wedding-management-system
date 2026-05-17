7feae26747c1276acf940a87c69e3ec7cdb4cc2a
het3812 <hetjoshi3812@gmail.com>
2026-05-18 00:35:59 +0530

    Connected Railway MySQL

--- db.py
+++ db.py
@@ -48,3 +48,28 @@ def execute_update(query, params=None):
     finally:
         cursor.close()
         conn.close()
+
+
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = mysql.connector.connect(
+    host=os.getenv("MYSQLHOST"),
+    port=os.getenv("MYSQLPORT"),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database=os.getenv("MYSQLDATABASE")
+)
+
+cursor = connection.cursor(dictionary=True)
+
+def execute_query(query, values=None):
+    cursor.execute(query, values or ())
+    return cursor.fetchall()
+
+def execute_update(query, values=None):
+    cursor.execute(query, values or ())
+    connection.commit()
